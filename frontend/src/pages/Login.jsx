8780b2da12874c73c7c7ea124e49dfd7e5f5eadc
sougata2-9 <sougatavscode9@gmail.com>
2025-08-30 08:38:45 -0700

    first commit from vs code

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const Login = () => {
+    return (
+   
+   <p>jhgrfjhgerf</p>
+  )
+}
+
+export default Login
---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import cors from 'cors'
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is Running on port ${process.env.PORT}`)
+}) 
